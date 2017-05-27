12b9a8f227a590f672062dedbd0da2e99465c7d1
Ross117 <RossMorran@hotmail.co.uk>
2017-05-27 16:55:11 +0100

    Give user option to tweet quote

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ $(".newQuote").on("click", function () {
 
     // make an API request to get a randomly generated quote
   $.ajax({
-    url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
     // call function if request succeeds
     success: function (json) {
       var quote = "",
@@ -32,6 +32,19 @@ $(".newQuote").on("click", function () {
 $(".tweetQuote").on("click", function () {
   "use strict";
 
-//   use the Twitter API to allow user to tweet a quote
+  var $quote = "",
+      link = "",
+      defaultMsg = "";
+
+  $quote = $(".quote").text();
+  defaultMsg = "Click the button to get a quote!";
+
+  if ($quote === "" || $quote === defaultMsg) return;
+  if ($quote.length > 140) return alert("Sorry, this quote is too long to tweet.");
+
+  link = "https://twitter.com/home?status=" + encodeURIComponent($quote);
+
+  //   give user the option to tweet a quote
+  window.open(link);
 
 });
