c8fb0dc1bb0004d61411234a390ccd039201616e
Ross117 <RossMorran@hotmail.co.uk>
2017-06-07 19:55:27 +0100

    remove alert when quote is too long to tweet, disable tweet quote button instead. Include quote marks and citation in tweet. Add ES6 elements to js code.

--- javascript.js
+++ javascript.js
@@ -1,29 +1,30 @@
-$(".newQuote").on("click", function () {
+$(".newQuote").on("click", () => {
   "use strict";
 
-  var $quoteInput = $(".quote"),
-    $citationInput = $(".citation");
+  const $quoteInput = $(".quote"),
+        $citationInput = $(".citation");
 
-
-  // filter out quotes over 140 chrs?
-
-    // make an API request to get a randomly generated quote
+  // make an API request to get a randomly generated quote
   $.ajax({
     url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
     // call function if request succeeds
-    success: function (json) {
-      var quote = "",
-          author = "";
-
+    success: (json) => {
       // deal with lsep issue - seen on quote from Apple
-      quote = json[0].content;
-      author = json[0].title;
-
+      const quote = json[0].content;
+      const author = json[0].title;
       $quoteInput.html(quote);
       $citationInput.html(author);
+      const tweet = getTweet();
+      const $twtBtn = $(".tweetQuote");
+//    if quote + citation length is 140 chrs or less, enable the tweet quote button
+      if (tweet.length <= 140) {
+        $twtBtn.prop("disabled", false);
+      } else {
+        $twtBtn.prop("disabled", true);
+      }
     },
     // handle request failure
-    error: function() {
+    error: () => {
       $quoteInput.html("Error");
       $citationInput.html("Sorry, something went wrong");
     },
@@ -32,23 +33,26 @@ $(".newQuote").on("click", function () {
 
 });
 
-$(".tweetQuote").on("click", function () {
+$(".tweetQuote").on("click", () => {
   "use strict";
 
-  var $quote = "",
-      link = "",
-      defaultMsg = "";
-
-  $quote = $(".quote").text();
-  defaultMsg = "Click the button to get a quote!";
-
-  // validate that quote can be tweeted
-  if ($quote === "" || $quote === defaultMsg) return;
-  if ($quote.length > 140) return alert("Sorry, this quote is too long to tweet.");
-
-  link = "https://twitter.com/home?status=" + encodeURIComponent($quote);
-
+  const tweet = getTweet();
+  //   handle errors
+  if (tweet === false) return;
+  const link = "https://twitter.com/home?status=" + tweet;
   //   give user the option to tweet a quote
   window.open(link);
 
 });
+
+function getTweet () {
+  "use strict";
+
+//   return the quote + the citation
+  const $quote = $(".quote").text(),
+        $citation = $(".citation").text(),
+        defaultMsg = "Click the button to get a quote about design!";
+  if ($quote === "" || $quote === defaultMsg) return false;
+  return '"' + $quote.trim() + '" ' + $citation;
+
+}
