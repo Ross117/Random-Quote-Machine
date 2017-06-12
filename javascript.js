ead530f0513bf90597febac40fdc87d1200ae100
Ross117 <RossMorran@hotmail.co.uk>
2017-06-12 23:56:28 +0100

    Centre HTML elements on the web page

--- javascript.js
+++ javascript.js
@@ -9,14 +9,18 @@ $(".newQuote").on("click", () => {
     url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
     // call function if request succeeds
     success: (json) => {
-      // deal with lsep issue - seen on quote from Apple
+      // need to deal with lsep issue - seen on quote from Apple
       const quote = json[0].content;
       const author = json[0].title;
+
       $quoteInput.html(quote);
       $citationInput.html(author);
+
       const tweet = getTweet();
       const $twtBtn = $(".tweetQuote");
-//    if quote + citation length is 140 chrs or less, enable the tweet quote button
+
+      // if quote + citation length is 140 chrs or less,
+      // enable the tweet quote button
       if (tweet.length <= 140) {
         $twtBtn.prop("disabled", false);
       } else {
@@ -39,6 +43,7 @@ $(".tweetQuote").on("click", () => {
   const tweet = getTweet();
   //   handle errors
   if (tweet === false) return;
+
   const link = "https://twitter.com/home?status=" + tweet;
   //   give user the option to tweet a quote
   window.open(link);
@@ -48,11 +53,13 @@ $(".tweetQuote").on("click", () => {
 function getTweet () {
   "use strict";
 
-//   return the quote + the citation
+  //  return the quote + the citation
   const $quote = $(".quote").text(),
         $citation = $(".citation").text(),
         defaultMsg = "Click the button to get a quote about design!";
+
   if ($quote === "" || $quote === defaultMsg) return false;
+
   return '"' + $quote.trim() + '" ' + $citation;
 
 }
